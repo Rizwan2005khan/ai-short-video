e7a0093a088d44f96a2a5b41578ef8c46c9e141d
Rizwan Ullah <rizwanullahicp0306@gmail.com>
2026-02-05 14:36:12 +0500

    completed frontend

--- client/src/pages/Plan.tsx
+++ client/src/pages/Plan.tsx
@@ -1,8 +1,10 @@
+import Pricing from "../components/Pricing"
 
 const Plan = () => {
   return (
-    <div>
-      <h1>Plans</h1>
+    <div className="max-sm:py-10 sm:pt-20">
+      <Pricing />  
+      <p className="text-center text-gray-400 max-w-md text-sm my-14 mx-auto px-12">Create stunning images for just <span className="text-indigo-400 font-medium">5 credits</span> and generate immersive videos for <span className="text-indigo-400 font-medium">10 credits</span>.</p>    
     </div>
   )
 }
